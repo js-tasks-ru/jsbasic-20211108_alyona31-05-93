f403187daed0ec81e036a3f011640f493a1be6aa
Alyona Markelova <alyona31.05.93@gmail.com>
2021-11-15 06:58:44 +0300

    feat: Решение 5 задачи 1 модуля.

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,3 +1,8 @@
 function truncate(str, maxlength) {
   // ваш код...
+  if(str.length <= maxlength)
+    return str;
+  let substitute = "…";
+  let sliced = str.slice(0, maxlength - substitute.length);
+  return sliced + substitute;
 }
