ded305f22fbd8758e853e1d8d6424090f9f1c94b
Alyona Markelova <alyona31.05.93@gmail.com>
2021-11-15 06:43:27 +0300

    feat: Решение 4 задачи 1 модуля.

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,11 @@
 function checkSpam(str) {
   // ваш код...
+  let spamWords = ["1xbet", "xxx"];
+  var lowerStr = str.toLowerCase();
+  for(let word of spamWords) {
+    if (lowerStr.includes(word))
+      return true;
+  }
+
+  return false;
 }
