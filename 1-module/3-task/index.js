70adbd0cb1f82ca363f5a3985ddbdc5ab888ba0d
Alyona Markelova <alyona31.05.93@gmail.com>
2021-11-15 06:34:47 +0300

    feat: Решение 2 и 3 задач 1 модуля.

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -1,3 +1,9 @@
 function ucFirst(str) {
-  // ваш код...
+  // ваш код...если строка не пустая, то делать преобразования,
+  //иначе выводить её же без преобразований, просто return?
+  if (str != null && str !== "" && str.trim() != "") {
+    return str[0].toUpperCase() + str.slice(1);
+  } else {
+    return str;
+  }
 }
