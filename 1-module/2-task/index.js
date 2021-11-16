8a034410deea1cde24fa30fad50dfb1973dea0fc
Alyona Markelova <alyona31.05.93@gmail.com>
2021-11-16 23:16:21 +0300

    refactor: Исправление 2 задачи 1 модуля.

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -10,7 +10,7 @@ function print(text) {
  * чтобы функция sayHello работала корректно
  */
 function isValid(name) {
-  if(name != '' && name != null && (name.indexOf(' ') == -1) && name.length>=4){  //name.includes(' ') - ???
+  if(name != '' && name != null && !name.includes(' ') && name.length>=4){
     return true;
   }else {
     return false;
