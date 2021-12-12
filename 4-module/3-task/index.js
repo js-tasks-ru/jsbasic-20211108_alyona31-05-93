a4b5f2dedce90a902f8feda29e4cb9b4dcf5b1b6
Alyona Markelova <alyona31.05.93@gmail.com>
2021-12-12 18:30:51 +0300

    feat: Добавление решения 3-й задачи 4-го модуля.

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,37 @@
 function highlight(table) {
-  // ваш код...
+  for (let i = 0; i < table.rows.length; i++) {
+    let row = table.rows[i];
+    for (let j = 0; j < row.cells.length; j++) {
+      let cell = row.cells[j];
+      let header = table.rows[0].cells[j].innerText;
+
+      switch (header) {
+      case "Status":
+        let isAvailable = cell.getAttribute("data-available");
+        if (!isAvailable) {
+          row.hidden = true;
+        } else {
+          if (isAvailable === "true") {
+            row.classList.add("available");
+          } else if (isAvailable === "false") {
+            row.classList.add("unavailable");
+          }
+        }
+        break;
+      case "Gender":
+        let gender = cell.innerText;
+        if (gender === "m") {
+          row.classList.add("male");
+        } else if (gender === "f") {
+          row.classList.add("female");
+        }
+        break;
+      case "Age":
+        if (cell.innerText < 18) {
+          row.style.textDecoration = "line-through";
+        }
+        break;
+      }
+    }
+  }
 }
