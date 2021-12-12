ac6f788b5a1744df3c66526d64bcd70d523c3745
Alyona Markelova <alyona31.05.93@gmail.com>
2021-12-12 22:58:19 +0300

    feat: Решение 3-й задачи 5-го модуля.

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,50 @@
 function initCarousel() {
-  // ваш код...
+  const rightButton = document.querySelector("button, .carousel__arrow_right");
+  const leftButton = document.querySelector("button, .carousel__arrow_left");
+  const carousel = document.querySelector(".carousel__inner");
+
+  const carouselOffset = carousel.offsetWidth; // ширина слайда
+  const carouselItemsCount = carousel.querySelectorAll(".carousel__slide").length; // кол-во слайдов
+  const rightBorder = 0;
+  const leftBorder = -1 * carouselOffset * (carouselItemsCount - 1);
+
+  const regex = /translateX\((?<translateX>-?\d+)px\)/;
+
+  leftButton.style.display = 'none';
+
+  rightButton.onclick = () => {
+    leftButton.style.display = ''; // левая кнопка будет доступна
+
+    let translateX = 0;
+
+    let currentTransform = carousel.style.transform;
+    if (regex.test(currentTransform)) {
+      translateX = Number(regex.exec(currentTransform).groups.translateX);
+    }
+
+    let offset = translateX - carouselOffset;
+    carousel.style.transform = `translateX(${offset}px)`;
+
+    if (offset <= leftBorder) {
+      rightButton.style.display = 'none';
+    }
+  };
+
+  leftButton.onclick = () => {
+    rightButton.style.display = ''; // правая кнопка будет доступна
+
+    let translateX = 0;
+
+    let currentTransform = carousel.style.transform;
+    if (regex.test(currentTransform)) {
+      translateX = Number(regex.exec(currentTransform).groups.translateX);
+    }
+
+    let offset = translateX + carouselOffset;
+    carousel.style.transform = `translateX(${offset}px)`;
+
+    if (offset >= rightBorder) {
+      leftButton.style.display = 'none';
+    }
+  };
 }
