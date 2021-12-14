c44582c5a0f87bf956b27958f64802b86ee9095c
Alyona Markelova <alyona31.05.93@gmail.com>
2021-12-15 00:51:13 +0300

    feat: Решение 2-й задачи 6-го модуля.

--- 6-module/2-task/index.js
+++ 6-module/2-task/index.js
@@ -1,4 +1,41 @@
+import createElement from '../../assets/lib/create-element.js';
+
 export default class ProductCard {
-  constructor(product) {
+  #productAddEvent = null;
+  #container = null;
+
+  get elem() {
+    return this.#container;
   }
+
+  constructor(product) {
+    this.#productAddEvent = new CustomEvent("product-add", {
+      detail: product.id,
+      bubbles: true
+    });
+
+    this.#container = this.#createContainer(product);
+  }
+
+  #createContainer = (product) => {
+    let container = createElement(this.#getDivLayout(product));
+    container.querySelector('.card__button').addEventListener('click', () => this.#container.dispatchEvent(this.#productAddEvent));
+
+    return container;
+  };
+
+  #getDivLayout = (product) => `
+    <div class="card">
+      <div class="card__top">
+        <img src="/assets/images/products/${product.image}" class="card__image" alt="product">
+        <span class="card__price">€${product.price.toFixed(2)}</span>
+      </div>
+      <div class="card__body">
+        <div class="card__title">${product.name}</div>
+        <button type="button" class="card__button">
+          <img src="/assets/images/icons/plus-icon.svg" alt="icon">
+        </button>
+      </div>
+    </div>
+  `;
 }
