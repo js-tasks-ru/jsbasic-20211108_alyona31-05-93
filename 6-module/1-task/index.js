31df22a6606bb6b15e606dc6dbadcaa2163e5a0c
Alyona Markelova <alyona31.05.93@gmail.com>
2021-12-15 00:14:06 +0300

    fix: Решение 1-й задачи 6-го модуля.

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -13,6 +13,57 @@
  *
  */
 export default class UserTable {
+  #items = null;
+  elem = null;
+
   constructor(rows) {
+    let id = 0;
+    this.elem = document.createElement('table');
+    this.#items = rows.map(row => {
+      return {row: row, id: id++};
+    });
+    this.#render();
   }
+
+  #render() {
+    this.elem.innerHTML = this.#renderTableContent(this.#items);
+
+    //#region клики на кнопки
+    let buttons = this.elem.querySelectorAll('.remove-row__button');
+    for (let i = 0; i < buttons.length; i++) {
+      buttons[i].addEventListener('click', this.#onRemoveRowButtonClick);
+    }
+    //#endregion
+  }
+
+  #onRemoveRowButtonClick = (event) => {
+    let toDelete = this.#items.filter(item => item.id === Number(event.currentTarget.id));
+    this.#items.splice(toDelete, 1);
+    this.#render();
+  };
+
+  //region Отрисовка таблицы
+  #renderTableContent = (items) => `
+      <thead>
+          <tr>
+              <th>Имя</th>
+              <th>Возраст</th>
+              <th>Зарплата</th>
+              <th>Город</th>
+              <th></th>
+          </tr>
+      </thead>
+      <tbody>
+          ${items.map(item => this.#renderItem(item.row, item.id)).join('')}
+      </tbody>`;
+
+  #renderItem = (item, id) => `
+      <tr>
+          <td>${item.name}</td>
+          <td>${item.age}</td>
+          <td>${item.salary}</td>
+          <td>${item.city}</td>
+          <td><button class="remove-row__button" id="${id}">X</button></td>
+      </tr>`;
+  //endregion
 }
